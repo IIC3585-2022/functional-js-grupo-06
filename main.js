4b320fd16ee9d87e00414bf165965b18137697f3
perezpefaur <perezpefaur@uc.cl>
2022-03-27 16:55:44 -0300

    Linter fixes and functional fixes

--- main.js
+++ main.js
@@ -1,85 +1,69 @@
 const prompt = require('prompt-sync')();
 var _ = require('lodash');
 
-throwsRecursion = (throwArray, current_score) => {
-    if ((current_score === 0) || (throwArray.length === 0)) return current_score
-
-    const th = _.head(throwArray)
-
-    if (th == 'DB') {
-        new_score = current_score - 50
-    }
-    else if (th == 'SB') {
-        new_score = current_score - 25
-    }
-    else {
-        parsed_th = JSON.parse(th);
-        new_score = current_score - (parsed_th[0] * parsed_th[1])
-    }
-
-    return throwsRecursion(_.tail(throwArray), new_score);
+function play_game(...args) {
+    // empezamos el juego con los jugadores correspondientes
+    const players = init_game(...args);
+    // iterator
+    console.log(`Juego inicializado con jugadores ${players.map(player => player[0])}.`);
+    console.log(players);
+    // llamamos a los turnos recursivamente
+    return recursive_turn(players);
 }
 
-turn_action = (nick, current_score, throws) => {
-    console.log(current_score);
-    console.log(throws);
-    console.log('holaaa');
-    //Iterator
+function init_game(...args){
+    // Currying
+    const playerHash501 = playerHash(501);
+    // Generator
+    return Array.from(args, playerHash501);
+}
 
-    new_score = throwsRecursion(throws, current_score)
+playerHash = (score) => nick => [nick, score];
+
+function recursive_turn(players){
+    // Generator
+    return recursive_turn( Array.from(players, update_list));
+}
+
+function update_list(x){
+    console.log(`Ingrese el lanzamiento de ${x[0]}:`);
+    // Combinator
+    return playerHash(turn_action(x[0], x[1], input_turn()))(x[0]);
+}
+
+function turn_action(nick, old_score, throws){
+
+    const new_score = throwsRecursion(throws, old_score);
 
     if (new_score === 0) {
-        console.log(`${nick} queda con 0 puntos y gana el juego. ¡Felicitaciones ${nick}!`)
+        console.log(`${nick} queda con 0 puntos y gana el juego. ¡Felicitaciones ${nick}!`);
         process.exit();
     }
 
     console.log(`${nick} tiene ${new_score} puntos.`);
-    return new_score
+    return new_score;
 }
 
-init_game = (...args) => {
-    let players = [];
-    console.log(args);
-    //Iterator
-    args.forEach(arg => 
-        players.push(playerHash501(arg)));
-        //Currying
-    return players
-}
-
-playerHash = (score => (nick => [nick, score]))
-playerHash501 = playerHash(501)
-
-
-
-play_game = (...args) => {
-    players = init_game(...args)
-    console.log(`Juego inicializado con jugadores ${players.map(player => player[0])}.`);
-    console.log(players);
-    recursive_turn(players);
-}
-
-
-recursive_turn = (players) => {
-    new_players = [];
-    //Iterator
-    players.map(player => {
-        nick = player[0];
-        current_score = player[1];
-        console.log(`Ingrese el lanzamiento de ${nick}:`);
-        //Combinator
-        new_score = turn_action(player[0], player[1], input_turn());
-        new_players.push(playerHash(new_score)(nick));
-    });
-    recursive_turn(new_players);
-}
-
-input_turn = () => {
+function input_turn(){
     //El input tiene que ser de esta forma DB;[3,20];[3,19]
-    var scores = prompt().split(';');
-    console.log(scores);
-    //Escribir un checkeo que revise que el input sea correcto
-    return scores
-}   
+    return prompt().split(';');
+}
 
-play_game("Juan", "Diego","Felipe")
+throwsRecursion = (throwArray, current_score) => {
+    if ((current_score === 0) || (throwArray.length === 0)) return current_score;
+
+    const th = _.head(throwArray);
+
+    if (th == 'DB') {
+        return throwsRecursion(_.tail(throwArray), current_score - 50);
+    }
+    else if (th == 'SB') {
+        return throwsRecursion(_.tail(throwArray), current_score - 25);
+    }
+    else {
+        const parsed_th = JSON.parse(th);
+        return throwsRecursion(_.tail(throwArray), current_score - (parsed_th[0] * parsed_th[1]));
+    }
+};
+
+play_game("Juan", "Diego","Felipe");
