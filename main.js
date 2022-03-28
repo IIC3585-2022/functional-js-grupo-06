585b5fea2ec269383eb657d49ff82a04d95be385
aigou42 <rheiremans@uc.cl>
2022-03-28 12:34:50 -0300

    🔒️ No acepta inputs correctos

--- main.js
+++ main.js
@@ -63,11 +63,90 @@ recursive_turn = (players) => {
     recursive_turn(new_players);
 }
 
+const compose = (f,g) => (x) => f(g(x))
+
 input_turn = () => {
     //El input tiene que ser de esta forma DB;[3,20];[3,19]
-    var scores = prompt().split(';');
+    while (true) {
+        var scores = prompt().split(';');
+        if (hasThreeItems( scores )) {
+            const numberPipe = compose(isList, hasTwoItems) //Compose
+            var goodInputs = 0;
+            scores.forEach(th => {
+                if ( isLuckySHot(th)) {
+                    goodInputs += 1;
+                } else if (numberPipe(th)){
+                    var points = th.replace("[", "").replace("]", "").replace(" ", "").split(',');
+                    var goodThrow = 0;
+                    points.forEach(number => {
+                        if ( isNumberinRange(3, number) || isNumberinRange(20, number)){
+                            goodThrow += 1;
+                        }
+                    })
+                    if (goodThrow === 2){
+                        goodInputs += 1;
+                    }
+                } else {
+                    console.log("Error en", th,"Cada lanzamiento debe tener el formato '[1,2]', 'SB' o 'DB' ")
+                }
+            }) 
+            if ( goodInputs === 3){
+                break;
+            }
+        }
+    }
     //Escribir un checkeo que revise que el input sea correcto
     return scores
 }
 
+hasThreeItems = ( scores ) => {
+    if ( _.size(scores) === 3 ){          //lodash
+        return scores
+    } else {
+        console.log("Su input debe tener el formato A;B;C");
+        return false;
+    }
+}
+
+isList = ( input ) => {
+    if ( input[0] === '[' && _.last(input) === ']' ) {  // lodash
+        return input;
+    }
+    return false;
+} 
+
+// _.isString( input )
+
+isLuckySHot = ( th ) => {
+    if ( th === "SB" || th === "DB") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+hasTwoItems = ( th ) => {
+    if ( _.size(th.split(',')) === 2 ) {
+        return th;
+    } else {
+        console.log("Error en", th,": Cada lanzamiento debe tener 2 elementos")
+        return false;
+    }
+}
+
+isNumberinRange = ( range, number ) => {
+    if ( !isNaN(number)){
+        if ( 1 <= Number(number) <= range) {
+            return true;
+        } else {
+            console.log("Error en", number,": Ingreso un lanzamiento inválido en la casilla donde debería ir en el rango de 1 a " + range);
+            return false;
+        }
+    } else {
+        console.log("Error en", number,": Ambos items de cada lanzamiento deben ser números")
+        return false;
+    }
+}
+
 play_game("Juan", "Diego","Felipe");
+
